914c5e486a1a76076041c30c3177feb32ad43a69
CAPACITACION-10 <sebasfernandez29@gmail.com>
2017-08-29 16:34:38 -0500

    Commit

---
+++ app/src/main/java/bool/myappjuanp/Main2Activity.java
@@ -0,0 +1,32 @@
+package bool.myappjuanp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private Button BLogin; // Crear variable
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        BLogin = (Button) findViewById(R.id.BLogin);
+        BLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Acciones
+
+                Intent loadActivity = new Intent(getApplicationContext(), MainActivity.class);
+
+                startActivity(loadActivity);
+            }
+        });
+    }
+
+}
